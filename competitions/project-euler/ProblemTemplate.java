0b98c1eaf27f35d9e6a3d0a98d71172ba4fe5803
Danial Goodwin <danialgoodwin@gmail.com>
2014-08-30 22:19:52 -0400

    Complete Project Euler problem 1

---
+++ competitions/project-euler/ProblemTemplate.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ */
+public class Problem1 {
+
+    public static void main(String[] args) {
+        int answer = solve();
+        System.out.println("P# Answer: " + answer);
+    }
+    
+    /** Solves problem in O() time and O() space. */
+    public static int solve() {
+    
+    }
+
+}

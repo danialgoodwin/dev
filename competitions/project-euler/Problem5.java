3a0720d3f84adeea6f2245ae11f13da2b5d1abf7
Danial Goodwin <danialgoodwin@gmail.com>
2014-09-18 10:51:25 -0400

    Write pseudocode and comments for PE5

--- competitions/project-euler/Problem5.java
+++ competitions/project-euler/Problem5.java
@@ -19,10 +19,27 @@ public class Problem5 {
     }
     
     public static int solveViaBruteForce() {
+        // Idea 0: Keep incrementing by 1 (or 20/max) and test all numbers for remainder is zero.
         // Idea 1: Create sieve.
         // Idea 2: Find Least Common Denominator algo.
         // Idea 3: Find common factors, and multiple.
+        // Idea 4: Euclid's Algorithm
+        // Idea 5: Count factors in a more algorithmic way by iterating through and up.
         
+    }
+    
+    public static int solveViaCountDivisors() {
+        // 1. Have a mutable list of all numbers that need to be evenly divided. // O(n) space, probably use LinkedList.
+        // 2. Remove all ones.
+        // 3. Divide all that are evenly divible by 2, record 2, repeat until no other records can be evenly divided by 2. // O(n) space.
+        // 4. Repeat 2-3 for increasing primes.
+        // 5. Multiply all recorded numbers that were evenly divisible, which will be the LCM.
+    }
+    
+    public static int solveViaCompareAllFactors() {
+        // 1. Have a list of all numbers that need to be evenly divided.
+        // 2. Find and save factors for all of the numbers. // O(k*n) space.
+        // 3. Iterate through all the saved factors, multiply the answer by factors with the most for their number.
     }
 
 }

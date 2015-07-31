c5f9b12cc7731aa2aae3164a1ee35173d1b59405
Danial Goodwin <danialgoodwin@gmail.com>
2015-07-30 23:13:37 -0400

    Add ImageMagick and PNG Crush scripts

---
+++ media/pngcrush.bat
@@ -0,0 +1,9 @@
+:: Run this file to minimize PNG size using pngcrush.
+:: Use `pngcrush -h` for more help options.
+
+:: For Windows command prompt
+..\pngcrush_1_7_85_w64.exe -brute -d ..\crushed *.png
+
+
+:: For bash terminal, not the difference in slash directions.
+:: ../pngcrush_1_7_85_w64.exe -brute -d ../crushed *.png

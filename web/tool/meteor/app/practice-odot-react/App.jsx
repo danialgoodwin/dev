0108b72e04055ecbdfa6252137e0637dfc41a8aa
Danial Goodwin <danialgoodwin@gmail.com>
2015-11-02 16:27:11 -0500

    Practice Meteor with React

---
+++ web/tool/meteor/app/practice-odot-react/App.jsx
@@ -0,0 +1,58 @@
+// More info: https://www.meteor.com/tutorials/react/update-and-remove
+//
+// App component - represents the whole app
+App = React.createClass({
+
+  // This mixin makes the getMeteorData method work
+  mixins: [ReactMeteorData],
+
+  // Loads items from the Tasks collection and puts them on this.data.tasks
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+    }
+  },
+
+  renderTasks() {
+    // Get tasks from this.data.tasks
+    return this.data.tasks.map((task) => {
+      return <Task key={task._id} task={task} />;
+    });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+});
---
+++ web/tool/meteor/app/practice-odot-react/practice-odot-react.jsx
@@ -0,0 +1,11 @@
+// Define a collection to hold our tasks
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code is executed on the client only
+
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    React.render(<App />, document.getElementById("render-target"));
+  });
+}

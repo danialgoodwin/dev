23f0543009bf8218314285d9ca9b53acdca439df
Danial Goodwin <danialgoodwin@gmail.com>
2016-01-14 15:03:58 -0500

    Add sample project for Node+Express+Mongo

---
+++ web/tool/node-js/app-sample/node-express-mongo-page/readme.md
@@ -0,0 +1,12 @@
+# How to build a simple web app with Node, Express, and Mongo DB
+
+Source: Meh, getting old: [Creating a Simple RESTful Web App with NODE.JS, EXPRESS, AND MONGODB](http://cwbuecheler.com/web/tutorials/2014/restful-web-app-node-express-mongodb/)
+
+1. Install Node and npm.
+2. Run `npm install -g express-generator`
+3. Run `express node-express-mongo-page`. (Or, whatever project name you want.)
+4. Add Mongo and Monk to package.json dependencies.
+5. Run `npm install`.
+6. Match the rest of the repo.
+7. Run `mongo` to start the MongoDB process
+8. Run `npm start`.
---
+++ web/tool/node-js/app-sample/node-express-mongo-page/routes/users.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+module.exports = router;

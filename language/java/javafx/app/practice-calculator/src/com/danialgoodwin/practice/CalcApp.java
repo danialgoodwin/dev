7d80c241b76503b583dab522829754a5c95f8ce8
Danial Goodwin <danialgoodwin@gmail.com>
2016-05-22 13:28:00 -0400

    Refactor JavaFx practice app

---
+++ language/java/javafx/app/practice-calculator/src/com/danialgoodwin/practice/CalcApp.java
@@ -0,0 +1,7 @@
+package com.danialgoodwin.practice;
+
+/**
+ * 2016-05-22: Created by Danial Goodwin
+ */
+public class CalcApp {
+}
--- language/java/javafx/app/practice-calculator/src/sample/Main.java
+++ language/java/javafx/app/practice-calculator/src/sample/Main.java
@@ -1,115 +1,36 @@
 package sample;
 
+import com.danialgoodwin.practice.CalcAppViaCode;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    private TextField num1Field, num2Field;
-    private Button addButton, divButton, subButton, mulButton, clearButton;
-    private Label answerLabel;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        num1Field = new TextField();
-        num2Field = new TextField();
-        addButton = new Button("+");
-        subButton = new Button("-");
-        mulButton = new Button("*");
-        divButton = new Button("/");
-        clearButton = new Button("Clear");
-        answerLabel = new Label("?");
-        answerLabel.setAlignment(Pos.CENTER);
-        answerLabel.setStyle("-fx-border-colof: #000; -fx-padding: 5px");
+    public void start(Stage primaryStage) throws Exception {
+        CalcAppViaCode calcAppViaCode = new CalcAppViaCode();
 
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         GridPane root = new GridPane();
         root.setAlignment(Pos.CENTER);
-        root.setHgap(10);
-        root.setVgap(10);
-        root.add(addButton, 0, 0);
-        root.add(subButton, 1, 0);
-        root.add(mulButton, 0, 1);
-        root.add(divButton, 1, 1);
-        root.add(num1Field, 0, 2);
-        root.add(num2Field, 1, 2);
-        root.add(answerLabel, 0, 3, 2, 1);
-        root.add(clearButton, 0, 4, 2, 1);
+        root.setHgap(20);
+        root.setVgap(20);
+        Button button = new Button("Calc via code");
+        root.add(button, 0, 0);
 
-        setWidths();
-        attachCode();
+        button.setOnAction(event -> calcAppViaCode.show());
 
         primaryStage.setTitle("Simple Calculator");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-    private void setWidths() {
-        num1Field.setPrefWidth(70);
-        num2Field.setPrefWidth(70);
-        addButton.setPrefWidth(70);
-        subButton.setPrefWidth(70);
-        mulButton.setPrefWidth(70);
-        divButton.setPrefWidth(70);
-        clearButton.setPrefWidth(150);
-        answerLabel.setPrefWidth(150);
-    }
-
-    private void attachCode() {
-        addButton.setOnAction(this::addButtonAction);
-        subButton.setOnAction(this::addButtonAction);
-        mulButton.setOnAction(this::addButtonAction);
-        divButton.setOnAction(this::addButtonAction);
-        clearButton.setOnAction(this::addButtonAction);
-    }
-
-    private void addButtonAction(ActionEvent e) {
-        if (e.getSource() == clearButton) {
-            num1Field.setText("");
-            num2Field.setText("");
-            answerLabel.setText("?");
-            num1Field.requestFocus();
-            return;
-        }
-        int answer = 0;
-        char symbol = ' ';
-        if (num1Field.getText().equals("")) { num1Field.setText("0"); }
-        if (num2Field.getText().equals("")) { num2Field.setText("0"); }
-        int num1 = parseIntOrElse(num1Field.getText(), 0);
-        int num2 = parseIntOrElse(num2Field.getText(), 0);
-        if (e.getSource() == addButton) {
-            symbol = '+';
-            answer = num1 + num2;
-        } else if (e.getSource() == subButton) {
-            symbol = '-';
-            answer = num1 - num2;
-        } else if (e.getSource() == mulButton) {
-            symbol = '*';
-            answer = num1 * num2;
-        } else if (e.getSource() == divButton) {
-            symbol = '/';
-            answer = num2 == 0 ? 0 : num1 / num2;
-        }
-        answerLabel.setText("" + num1 + symbol + num2 + "=" + answer);
-    }
-
-    private static int parseIntOrElse(String s, int defaultValue) {
-        try {
-            return Integer.parseInt(s);
-        } catch (NumberFormatException e) {
-            return defaultValue;
-        }
-    }
 
 }
